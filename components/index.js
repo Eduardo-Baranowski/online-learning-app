31041b693ba843bc61423062a150ab9c2baf1bac
Eduardo Baranowski <eduardobaranowski@gmail.com>
2022-03-06 11:58:46 -0300

    primeira parte finalizada

--- components/index.js
+++ components/index.js
@@ -5,8 +5,14 @@ import VerticalCourseCard from './VerticalCourseCard';
 import LineDivider from './LineDivider';
 import CategoryCard from './CategoryCard';
 import HorizontalCourseCard from './HorizontalCourseCard';
+import ProgressBar from './ProgressBar';
+import ProfileValue from './ProfileValue';
+import ProfileRadioButton from './ProfileRadioButton';
 
 export {
+  ProfileRadioButton,
+  ProfileValue,
+  ProgressBar,
   IconButton,
   TextButton,
   VerticalCourseCard,
