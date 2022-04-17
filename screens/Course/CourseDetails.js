58f12544bdf125df2f2b507577fa0cec0373f0d8
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2022-04-17 09:13:43 -0300

    Tabs de detalhes de curso

--- screens/Course/CourseDetails.js
+++ screens/Course/CourseDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {
   View,
   Text,
@@ -7,6 +7,7 @@ import {
   Animated,
   Keyboard,
 } from 'react-native';
+// import Video from 'react-native-video';
 
 import {IconButton, LineDivider} from '../../components';
 import {
@@ -18,8 +19,213 @@ import {
   dummyData,
 } from '../../constants';
 
+const course_details_tabs = constants.course_details_tabs.map(
+  course_details_tab => ({
+    ...course_details_tab,
+    ref: React.createRef(),
+  }),
+);
+
+const TabIndicator = ({measureLayout, scrollX}) => {
+  const inputRange = course_details_tabs.map((_, i) => i * SIZES.width);
+
+  const tabIndicatorWidth = scrollX.interpolate({
+    inputRange,
+    outputRange: measureLayout.map(measure => measure.width),
+  });
+
+  const translateX = scrollX.interpolate({
+    inputRange,
+    outputRange: measureLayout.map(measure => measure.x),
+  });
+
+  return (
+    <Animated.View
+      style={{
+        position: 'absolute',
+        bottom: 0,
+        height: 4,
+        width: tabIndicatorWidth,
+        borderRadius: SIZES.radius,
+        backgroundColor: COLORS.primary,
+        transform: [
+          {
+            translateX,
+          },
+        ],
+      }}
+    />
+  );
+};
+
+const Tabs = ({scrollX, onTabPress}) => {
+  const [measureLayout, setMeasureLayout] = useState([]);
+  const containerRef = React.useRef();
+
+  useEffect(() => {
+    let ml = [];
+
+    course_details_tabs.forEach(course_details_tab => {
+      course_details_tab?.ref?.current?.measureLayout(
+        containerRef.current,
+        (x, y, width, height) => {
+          ml.push({
+            x,
+            y,
+            width,
+            height,
+          });
+
+          if (ml.length === course_details_tabs.length) {
+            setMeasureLayout(ml);
+          }
+        },
+      );
+    });
+  }, [containerRef.current]);
+
+  return (
+    <View
+      ref={containerRef}
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+      }}>
+      {measureLayout.length > 0 && (
+        <TabIndicator measureLayout={measureLayout} scrollX={scrollX} />
+      )}
+      {course_details_tabs.map((item, index) => {
+        return (
+          <TouchableOpacity
+            key={`Tab-${index}`}
+            ref={item.ref}
+            style={{
+              flex: 1,
+              paddingHorizontal: 15,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            onPress={() => {
+              onTabPress(index);
+            }}>
+            <Text
+              style={{
+                ...FONTS.h3,
+                fontSize: SIZES.height > 800 ? 18 : 17,
+              }}>
+              {item.label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
 const CourseDetails = ({navigation, route}) => {
   const {selectedCourse} = route.params;
+
+  const [playVideo, setPlayVideo] = useState(false);
+
+  const flatListRef = React.useRef();
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+
+  const onTabPress = useCallback(tabIndex => {
+    flatListRef?.current?.scrollToOffset({
+      offset: tabIndex * SIZES.width,
+    });
+  });
+
+  function renderHeaderComponents() {
+    return (
+      <>
+        <View
+          style={{
+            flex: 1,
+          }}>
+          <IconButton
+            icon={icons.back}
+            iconStyle={{
+              width: 25,
+              height: 25,
+              tintColor: COLORS.black,
+            }}
+            containerStyle={{
+              width: 40,
+              height: 40,
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: 20,
+              backgroundColor: COLORS.white,
+            }}
+            onPress={() => navigation.goBack()}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+          }}>
+          <IconButton
+            icon={icons.media}
+            iconStyle={{
+              tintColor: COLORS.white,
+            }}
+            containerStyle={{
+              width: 50,
+              height: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          />
+          <IconButton
+            icon={icons.favourite_outline}
+            iconStyle={{
+              tintColor: COLORS.white,
+            }}
+            containerStyle={{
+              width: 50,
+              height: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          />
+        </View>
+      </>
+    );
+  }
+
+  function renderHeader() {
+    if (playVideo) {
+      return (
+        <View
+          style={{
+            flexDirection: 'row',
+            paddingHorizontal: SIZES.radius,
+            paddingBottom: SIZES.base,
+            height: 85,
+            backgroundColor: COLORS.black,
+            alignItems: 'flex-end',
+          }}>
+          {renderHeaderComponents()}
+        </View>
+      );
+    } else {
+      return (
+        <View
+          style={{
+            position: 'absolute',
+            top: SIZES.height > 800 ? 40 : 20,
+            left: 0,
+            right: 0,
+            flexDirection: 'row',
+            paddingHorizontal: SIZES.padding,
+            zIndex: 1,
+          }}>
+          {renderHeaderComponents()}
+        </View>
+      );
+    }
+  }
 
   function renderVideoSection() {
     return (
@@ -29,8 +235,100 @@ const CourseDetails = ({navigation, route}) => {
           alignItems: 'center',
           justifyContent: 'center',
           backgroundColor: COLORS.gray90,
-        }}
-      />
+        }}>
+        <ImageBackground
+          source={selectedCourse?.thumbnail}
+          style={{
+            width: '100%',
+            height: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <IconButton
+            icon={icons.play}
+            iconStyle={{
+              width: 25,
+              height: 25,
+              tintColor: COLORS.white,
+            }}
+            containerStyle={{
+              width: 55,
+              height: 55,
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginTop: SIZES.padding,
+              borderRadius: 30,
+              backgroundColor: COLORS.primary,
+            }}
+            onPress={() => setPlayVideo(true)}
+          />
+        </ImageBackground>
+        {/* {playVideo && (
+          <Video
+            source={{uri: dummyData?.sample_video_url}}
+            controls={true}
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              bottom: 0,
+              right: 0,
+              backgroundColor: COLORS.black,
+            }}
+          />
+        )} */}
+      </View>
+    );
+  }
+
+  function renderContent() {
+    return (
+      <View
+        style={{
+          flex: 1,
+        }}>
+        <View
+          style={{
+            height: 60,
+          }}>
+          <Tabs scrollX={scrollX} onTabPress={onTabPress} />
+        </View>
+        <LineDivider
+          lineStyle={{
+            backgroundColor: COLORS.gray20,
+          }}
+        />
+        <Animated.FlatList
+          ref={flatListRef}
+          horizontal
+          padingEnabled
+          snapToAlignment="center"
+          snapToInterval={SIZES.width}
+          decelerationRate="fast"
+          keyboardDismissMode="on-drag"
+          showsHorizontalScrollIndicator={false}
+          data={constants.course_details_tabs}
+          keyExtractor={item => `CourseDetailTabs-${item.id}`}
+          onScroll={Animated.event(
+            [{nativeEvent: {contentOffset: {x: scrollX}}}],
+            {
+              useNativeDriver: false,
+            },
+          )}
+          renderItem={({item, index}) => {
+            return (
+              <View
+                style={{
+                  width: SIZES.width,
+                }}>
+                {index === 0 && <Text>Chapters</Text>}
+                {index === 1 && <Text>Files</Text>}
+                {index === 2 && <Text>Discussions</Text>}
+              </View>
+            );
+          }}
+        />
+      </View>
     );
   }
 
@@ -40,7 +338,9 @@ const CourseDetails = ({navigation, route}) => {
         flex: 1,
         backgroundColor: COLORS.white,
       }}>
+      {renderHeader()}
       {renderVideoSection()}
+      {renderContent()}
     </View>
   );
 };
